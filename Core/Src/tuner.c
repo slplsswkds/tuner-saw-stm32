e4535f8ff0ed397ece44ebe0bc5d84e1a1bd2b51
slplsswkds <slplsswkds@protonmail.com>
2025-07-20 13:08:42 +0300

    fixed sendUartStr()

--- Core/Src/tuner.c
+++ Core/Src/tuner.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "arm_math.h"
 
 void blinkTimesWithDelay(const int times, const int delay)
 {
@@ -22,11 +23,17 @@ extern UART_HandleTypeDef huart1;
 
 volatile bool UART_TX_BUSY = false;
 
-void sendUartStr(const uint8_t* str)
-{
-    while (UART_TX_BUSY) { __NOP(); }
-    HAL_UART_Transmit_DMA(&huart1, str, strlen((char*)str));
-}
+void sendUartStr(const uint8_t* str);
+void startAdcDataRecording(uint32_t* pData);
+void waitForAdcData();
+void fft();
+void calculateStringTuningInfo();
+void showInfo();
+
+const uint32_t AUDIO_DATA_LEN = 1024;
+bool AUDIO_DATA_IS_ACTUAL = false;
+
+uint8_t UART_TX_DATA[128];
 
 int main(void)
 {
@@ -49,18 +56,15 @@ int main(void)
         blinkTimesWithDelay(5, 100);
     }
 
-    const unsigned int DATA_LEN = 1024;
-    uint16_t ADC_VAL[DATA_LEN];
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_VAL, 1280);
+    uint16_t ADC_VAL[AUDIO_DATA_LEN];
 
-    uint8_t TxData[128];
-    unsigned int test_counter = 0;
     while (1)
     {
-        snprintf((char*)TxData, sizeof(TxData), "QWERTY %u\n\r", test_counter);
-        sendUartStr(TxData);
-        HAL_Delay(500);
-        test_counter++;
+        startAdcDataRecording((uint32_t*)ADC_VAL);
+        waitForAdcData();
+        fft();
+        calculateStringTuningInfo();
+        showInfo();
     }
 }
 
@@ -70,4 +74,51 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart)
     {
         UART_TX_BUSY = false;
     }
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+    if (hadc->Instance == ADC1)
+    {
+        AUDIO_DATA_IS_ACTUAL = true;
+    }
+}
+
+void startAdcDataRecording(uint32_t* pData)
+{
+    HAL_ADC_Start_DMA(&hadc1, pData, 1024);
+}
+
+void waitForAdcData()
+{
+    HAL_SuspendTick();
+    HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+    HAL_ResumeTick();
+
+    snprintf((char*)UART_TX_DATA, sizeof(UART_TX_DATA), "Waked Up\n\r");
+    sendUartStr(UART_TX_DATA);
+}
+
+void fft()
+{
+}
+
+void calculateStringTuningInfo()
+{
+}
+
+void showInfo()
+{
+    snprintf((char*)UART_TX_DATA, sizeof(UART_TX_DATA), "QWERTY\n\r");
+    sendUartStr(UART_TX_DATA);
+    HAL_Delay(500);
+}
+
+void sendUartStr(const uint8_t* str)
+{
+    uint8_t internalUartTxData[sizeof(UART_TX_DATA)];
+    while (UART_TX_BUSY) { __NOP(); }
+    memccpy(internalUartTxData, str, '\0', sizeof(internalUartTxData));
+    UART_TX_BUSY = true;
+    HAL_UART_Transmit_DMA(&huart1, internalUartTxData, strlen((char*)str));
 }
